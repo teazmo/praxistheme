841e0bd4760a58a7a7aae3775e3c1909ec0a7a53
Timo Ihnken <timo.ihnken@googlemail.com>
2019-08-16 16:37:28 +0200

    Loop in index.php
    Eine Schleife um testweise Inhalt schon auf der Startseite auszugeben

--- index.php
+++ index.php
@@ -18,9 +18,15 @@
         </header><!-- #header -->
 
         <main>
-            <div id="content">
-              <h1>Blueprint theme</h1>
-            </div><!-- #content -->
+          <div id="content">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="article">
+                    <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                    <?php the_content(); ?>
+                </div> <!-- .article -->
+            <?php endwhile; endif; ?>
+          </div>
+            <!-- #content -->
 
             <aside id="sidebar">
             </aside><!-- #sidebar -->
