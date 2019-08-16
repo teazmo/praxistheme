fb20cb49ec88c98a700180a0e196ccbce649670b
Timo Ihnken <timo.ihnken@googlemail.com>
2019-08-16 15:40:44 +0200

    index.php und style.css erstellt
    In der Stylesheet sind generierte Klassen, die nicht mehr up to date sind

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title><?php wp_title(); ?> - <?php bloginfo('name'); ?></title>
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="wrapper">
+        <header>
+        </header><!-- #header -->
+
+        <main>
+            <div id="content">
+              <h1>Blueprint theme</h1>
+            </div><!-- #content -->
+
+            <aside id="sidebar">
+            </aside><!-- #sidebar -->
+
+        </main><!-- #main -->
+        <footer>
+        </footer><!-- #footer -->
+    </div><!-- #wrapper -->
+
+ <?php wp_footer(); ?>
+
+</body>
+</html>
