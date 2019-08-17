29aff3050481fbddf11f4d8c1f5f361ba3567130
Timo Ihnken <timo.ihnken@googlemail.com>
2019-08-17 15:30:59 +0200

    Page und Single hinzugefügt
    Page und Single Templates hinzugefügt.

---
+++ page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+            <h1>Page</h1>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="article">
+                    <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                    <?php the_content(); ?>
+                </div> <!-- .article -->
+            <?php endwhile; endif; ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<h1>Single</h1>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+            <div id="meta">
+                <span>geschrieben am: <?php the_date('d.m.Y'); ?>
+                von: <?php the_author(); ?> in
+                Kategorie(n): <?php the_category(', '); ?><?php the_tags(' und getagged mit: ', '', ''); ?></span>
+            </div>
+
+            <div class="entry">
+                <?php the_content(); ?>
+            </div>
+
+        <?php endwhile; endif; ?>
+
+        <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
