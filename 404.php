8a1dd7c210f980834bee4affbe60a81f81afbd59
Timo Ihnken <timo.ihnken@googlemail.com>
2019-08-20 17:22:50 +0200

    404 Seite angelegt
    Fehlerseite angelegt. Einfache Form. Muss noch mit genauen Hinweisen und gestaltetem Suchfeld umgesetzt werden

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div id="main">
+<div id="content">
+<h2>404 - Seite nicht gefunden</h2>
+<p>Diese Seite existiert nicht</p>
+<?php get_search_form(); ?>
+</div><!-- #content -->
+
+<div id="sidebar">
+<?php get_sidebar(); ?>
+</div><!-- #sidebar -->
+
+<div class="clear"></div>
+</div><!-- #main -->
+
+<?php get_footer(); ?>
