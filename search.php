75c595bf45492f80d9452e365e8d2782dc8abe43
Timo Ihnken <timo.ihnken@googlemail.com>
2019-08-20 17:33:39 +0200

    Create search.php

---
+++ search.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+  <div id="main">
+      <div id="content">
+      <div class="search-title"><strong>Suchergebnisse für: <span class="category-name"><?php echo get_search_query(); ?></span></strong></div>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <div class="article">
+      <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+      <?php the_content(); ?>
+      </div> <!-- .article -->
+      <?php endwhile; else: ?>
+      <p><?php _e('Entschuldigung. Deine Suche ergab keine Treffer.'); ?></p>
+      <?php endif; ?>
+      </div><!-- #content -->
+
+    <div id="sidebar">
+    <?php get_sidebar(); ?>
+    </div><!-- #sidebar -->
+
+    <div class="clear"></div>
+  </div><!-- #main -->
+<?php get_footer(); ?>
