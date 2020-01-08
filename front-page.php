49d29eccdee85677d91100818ce0caeadc0444c1
Timo Ihnken <timo.ihnken@googlemail.com>
2020-01-08 16:19:47 +0100

    Post Thumbnail vor Beiträgen bzw Teasern

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <div class="article">
                     <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                    <?php the_post_thumbnail('thumbnail', array('class' => 'card-image')); ?>
                     <?php the_content(); ?>
                 </div> <!-- .article -->
             <?php endwhile; endif; ?>
