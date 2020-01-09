1f42100f0b80c9edf79c1e7c02505b8315e87821
Timo Ihnken <timo.ihnken@googlemail.com>
2020-01-09 12:13:00 +0100

    Blogansicht für Startseite definiert
    HTML-Struktur asu Lernwerkstatt übernommen. Flexbox für Teaser auf Desktopansicht

--- front-page.php
+++ front-page.php
@@ -1,11 +1,23 @@
 <?php get_header(); ?>
-            <h1>Front-Page</h1>
+			<div class="blog-area">
+			<div class="blog-header">
+				<h2>Aktuelles</h2>
+				<button type="button">Alle anzeigen</button>
+			</div><!--.blog-header-->
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <div class="article">
-                    <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                    <?php the_post_thumbnail('thumbnail', array('class' => 'card-image')); ?>
-                    <?php the_content(); ?>
+					<div class="row">
+						<div class="col-left">
+							<?php the_post_thumbnail('thumbnail', array('class' => 'card-image')); ?>
+						</div><!--.col-left-->
+						<div class="col-right">
+							<h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+							<?php the_excerpt(); ?>
+							<a href="<?php the_permalink() ?>" class="more-button">Mehr …</a>
+						</div><!--.col-right-->
+					</div><!--.row-->
                 </div> <!-- .article -->
             <?php endwhile; endif; ?>
+			</div><!--.blog-area-->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
